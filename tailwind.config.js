7802be49c312fd7d1843c043fcd2c6634b5bb290
turnerburchard <tburchard4@gmail.com>
2025-03-09 12:14:03 -0600

    doing a big refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
     content: [
-        "./src/**/*.{js,jsx,ts,tsx}",
-        "./public/index.html",
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
     ],
     darkMode: 'class',
     theme: {
