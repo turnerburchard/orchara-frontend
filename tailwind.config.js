1e565614369c4600748af6cf470023218c0f77d7
turnerburchard <tburchard4@gmail.com>
2025-03-09 12:16:11 -0600

    doing a big refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
@@ -9,6 +11,6 @@ export default {
         extend: {},
     },
     plugins: [
-        require('@tailwindcss/forms'),
+        forms,
     ],
 };
