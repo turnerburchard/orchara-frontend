21f40068444063736f6e7cd7a9c99a4dccaa206f
turnerburchard <tburchard4@gmail.com>
2025-03-09 12:38:19 -0600

    doing a big refactor

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import ReactDOM from 'react-dom/client'
+import * as React from 'react'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 import './styles/tailwind.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
